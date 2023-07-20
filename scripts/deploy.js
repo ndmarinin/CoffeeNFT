21fca794fe649b455a19cc769542803f11d77780
Proshka766 <96373703+Proshka766@users.noreply.github.com>
2023-07-20 17:08:22 +0300

    delete getBalance

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,6 @@ async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
   const CoffeeToken = await ethers.getContractFactory("CoffeeToken");
   const coffeeToken = await CoffeeToken.deploy();
   console.log("Token address:", coffeeToken.address);
