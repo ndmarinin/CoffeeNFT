acbcb056013e420a20d3b8906b4e4ef9ae9f6a0e
Proshka766 <96373703+Proshka766@users.noreply.github.com>
2023-07-20 17:12:43 +0300

    fix contract name

--- contracts/CoffeeToken.sol
+++ contracts/CoffeeToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract CoffeeNFT is ERC721 {
+contract CoffeeToken is ERC721 {
     uint256 NFTPrice = 0.01 ether;
 
     constructor() ERC721("CoffeeNFT", "CFT") {}
