1d50788349f6bcecc9d141180924afd38d268ea2
Proshka766 <96373703+Proshka766@users.noreply.github.com>
2023-07-21 17:35:49 +0300

    fix main network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, API_MAIN_URL, PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
     siberium_main:{
-      url: API_URL,
+      url: API_MAIN_URL,
       accoutns: [PRIVATE_KEY]
     },
     siberium_test: {
