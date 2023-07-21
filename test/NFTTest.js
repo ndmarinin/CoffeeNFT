1e99fe9bf9bc549325d5d6041d19fdc75dfd18be
Proshka766 <96373703+Proshka766@users.noreply.github.com>
2023-07-21 19:06:07 +0300

    test works

--- test/NFTTest.js
+++ test/NFTTest.js
@@ -1,37 +1,24 @@
 const { expect } = require("chai");
 
 describe('CoffeeNFTtest', function() {
-    let CoffeeToken;
     let coffeeTokenAddress;
+    let coffeeToken;
+    let acc0, acc1, acc2, acc3, acc4, acc5;
 
     before(async () => {
         const CoffeeToken = await ethers.getContractFactory("CoffeeToken");
-        const coffeeToken = await CoffeeToken.deploy();
-        await coffeeToken.deployed();
-        coffeeTokenAddress = coffeeToken.address;
+        coffeeToken = await CoffeeToken.deploy();
+        coffeeTokenAddress = await coffeeToken.getAddress();
+        [acc0, acc1, acc2, acc3, acc4, acc5] = await ethers.getSigners();
+        
     });
 
     it("Should minting NFT", async function() {
-        await ecLibraryTest.mul('0x1');
-        const xCoordinate = await ecLibraryTest.hx();
-        const yCoordinate = await ecLibraryTest.hy();
-
-        const expectedX = '0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798';
-        const expectedY = '0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8';
-
-        expect(xCoordinate).to.equal(expectedX);
-        expect(yCoordinate).to.equal(expectedY);
-    });
-
-    it("Scalar Multiplication: 5G with Buterin trick", async function() {
-        await ecLibraryTest.ecMulV(
-            Gx,
-            Gy,
-            '0x5',
-            '0x2F8BDE4D1A07209355B4A7250A5C5128E88B84BDDC619AB7CBA8D569B240EFE4',
-            '0xD8AC222636E5E3D6D4DBA9DDA6C9C426F788271BAB0D6840DCA87D3AA6AC62D6'
+        await coffeeToken.connect(acc0).safeMintNFT(
+            acc1.address,
+            1,
+            { value: ethers.parseEther("0.01") }
         );
-        const success = await ecLibraryTest.equality();
-        expect(success).to.equal(true);
+        expect(await coffeeToken.balanceOf(acc1.address)).to.equal(1);
     });
 });
