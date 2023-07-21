14ee031e0054b54449d01f0935ee0c0c936a18b0
Proshka766 <96373703+Proshka766@users.noreply.github.com>
2023-07-21 22:01:29 +0300

    add gas to deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,10 @@
   
     console.log("Deploying contracts with the account:", deployer.address);
   
-    const token = await ethers.deployContract("CoffeeToken");
+    const token = await ethers.deployContract("CoffeeToken", {gas: ethers.parseEther("0.000001")});
+    const lock = await ethers.deployContract("Lock", [unlockTime], {
+      value: lockedAmount,
+    });
   
     console.log("Token address:", await token.getAddress());
   }
